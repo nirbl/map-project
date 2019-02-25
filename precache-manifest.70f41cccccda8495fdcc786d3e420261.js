d30b915982913ecf27cf76ffd6d01196f110c69b
nitbl <barzilai.nir@gmail.com>
2019-02-25 12:51:01 +0200

    Updates

---
+++ precache-manifest.70f41cccccda8495fdcc786d3e420261.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "986fd2e5b304465ab9ad",
+    "url": "/map-project/static/css/main.fe94d692.chunk.css"
+  },
+  {
+    "revision": "986fd2e5b304465ab9ad",
+    "url": "/map-project/static/js/main.986fd2e5.chunk.js"
+  },
+  {
+    "revision": "aafcb654d8a27fc99230",
+    "url": "/map-project/static/js/runtime~main.aafcb654.js"
+  },
+  {
+    "revision": "f88f5add2f5221c7c053",
+    "url": "/map-project/static/js/2.f88f5add.chunk.js"
+  },
+  {
+    "revision": "c70b3e2b0ffeab401504e03a74390447",
+    "url": "/map-project/index.html"
+  }
+];
